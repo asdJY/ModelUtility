7ed280bcabf77e0f507580e21379ff819d3d50d0
任贵权 <ansheck@163.com>
2016-05-09 15:16:48 +0800

    Initial Commit

---
+++ ModelUtility/ModelUtility.h
@@ -0,0 +1,19 @@
+//
+//  ModelUtility.h
+//  ModelUtility
+//
+//  Created by 任贵权 on 16/5/9.
+//  Copyright © 2016年 任贵权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModelUtility.
+FOUNDATION_EXPORT double ModelUtilityVersionNumber;
+
+//! Project version string for ModelUtility.
+FOUNDATION_EXPORT const unsigned char ModelUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModelUtility/PublicHeader.h>
+
+
